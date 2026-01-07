55f476c726985d4f2153a3fa7fa3ca69f65239f9
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-01-07 09:36:04 -0500

    Hello world

--- semana-1/stitch_javascript_practice_dashboard/script.js
+++ semana-1/stitch_javascript_practice_dashboard/script.js
@@ -1,14 +1,15 @@
 /* Counter */
-const counter = document.getElementById('counter')
+const counter = document.querySelector('#counter')
 const buttonAddCounter = document.getElementById('add-counter')
 const buttonSubtractCounter = document.getElementById('subtract-counter')
 const buttonResetCounter = document.getElementById('reset-counter')
+
 /* Counter events */
 function buttonsCounter(element, func){
     element.addEventListener('click', ()=>{
         switch (func) {
             case "add":
-                ++counter.innerText;
+                counter.innerText++;
                 break
             case "subtract":
                 --counter.innerText;
@@ -24,4 +25,18 @@ buttonsCounter(buttonAddCounter, "add")
 buttonsCounter(buttonSubtractCounter, "subtract")
 buttonsCounter(buttonResetCounter, "reset")
 
+/* Hello world generator */
+const nameGreeting = document.getElementById('name-greeting')
+const waittingBox = document.querySelector('body > div > main > div > div.grid.grid-cols-1.md\\:grid-cols-2.xl\\:grid-cols-3.gap-6 > div:nth-child(1) > div.p-5.flex.flex-col.gap-4.flex-1 > div > div') 
+console.log(waittingBox);
 
+nameGreeting.addEventListener("keyup", (e)=>{
+    if ((e.target.value).trim().length > 0) {
+        waittingBox.textContent = "Ok..."
+        waittingBox.style.backgroundColor = "limegreen"
+    }
+    else{
+        waittingBox.textContent = "Waiting for input..."
+        waittingBox.style.backgroundColor = "rgb(246 247 248 / var(--tw-bg-opacity, 1))"
+    }
+})
