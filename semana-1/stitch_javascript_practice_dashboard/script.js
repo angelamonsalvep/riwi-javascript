6111a53c305a0e5e5b88286ac91aa76cccf18354
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-01-07 11:22:34 -0500

    Hello world

--- semana-1/stitch_javascript_practice_dashboard/script.js
+++ semana-1/stitch_javascript_practice_dashboard/script.js
@@ -5,15 +5,15 @@ const buttonSubtractCounter = document.getElementById('subtract-counter')
 const buttonResetCounter = document.getElementById('reset-counter')
 
 /* Counter events */
-function buttonsCounter(element, func){
-    element.addEventListener('click', ()=>{
+function buttonsCounter(element, func) {
+    element.addEventListener('click', () => {
         switch (func) {
             case "add":
                 counter.innerText++;
                 break
             case "subtract":
                 --counter.innerText;
-                break 
+                break
             case "reset":
                 counter.innerText = 0;
                 break
@@ -27,15 +27,25 @@ buttonsCounter(buttonResetCounter, "reset")
 
 /* Hello world generator */
 const nameGreeting = document.getElementById('name-greeting')
-const waittingBox = document.querySelector('body > div > main > div > div.grid.grid-cols-1.md\\:grid-cols-2.xl\\:grid-cols-3.gap-6 > div:nth-child(1) > div.p-5.flex.flex-col.gap-4.flex-1 > div > div') 
-console.log(waittingBox);
+const waittingBox = document.querySelector('body > div > main > div > div.grid.grid-cols-1.md\\:grid-cols-2.xl\\:grid-cols-3.gap-6 > div:nth-child(1) > div.p-5.flex.flex-col.gap-4.flex-1 > div > div')
+const buttonGreet = document.querySelector('body > div > main > div > div.grid.grid-cols-1.md\\:grid-cols-2.xl\\:grid-cols-3.gap-6 > div:nth-child(1) > div.p-5.flex.flex-col.gap-4.flex-1 > div > button')
 
-nameGreeting.addEventListener("keyup", (e)=>{
+
+nameGreeting.addEventListener("keyup", (e) => {
     if ((e.target.value).trim().length > 0) {
         waittingBox.textContent = "Ok..."
         waittingBox.style.backgroundColor = "limegreen"
+        buttonGreet.addEventListener('click', () => {
+            // buttonGreet.setAttribute()
+            if ((e.target.value).trim().length > 0) {
+                alert(`Bienvenido ${e.target.value}`);
+                e.target.value = null
+                waittingBox.textContent = "Waiting for input..."
+                waittingBox.style.backgroundColor = "rgb(246 247 248 / var(--tw-bg-opacity, 1))"
+            }
+        })
     }
-    else{
+    else {
         waittingBox.textContent = "Waiting for input..."
         waittingBox.style.backgroundColor = "rgb(246 247 248 / var(--tw-bg-opacity, 1))"
     }
