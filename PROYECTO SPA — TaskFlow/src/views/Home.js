8dea8a511373bf77b8cb1946691e057fcc2663a3
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-01-16 10:28:49 -0500

    Welcome Home

--- PROYECTO SPA — TaskFlow/src/views/Home.js
+++ PROYECTO SPA — TaskFlow/src/views/Home.js
@@ -1,3 +1,9 @@
+import { store } from "../store/store.js"
+
 export function Home (){
-    return `Este es Home`
+    return `
+    <div class="h-[calc(100vh-40px)] flex flex-col justify-center items-center">
+        <h1 class="text-6xl">Bienvenido</h1>
+        <p class="text-3xl">${sessionStorage.getItem('user')}</p>
+    </div>`
 }
