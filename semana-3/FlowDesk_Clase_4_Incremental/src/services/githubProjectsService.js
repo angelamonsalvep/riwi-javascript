0862eb8f19ed3c06be97b348f3b6112f07083be2
angela MOnsalve <angelapmonsalve@gmail.com>
2026-01-20 20:34:02 -0500

    add simulacrum

---
+++ semana-3/FlowDesk_Clase_4_Incremental/src/services/githubProjectsService.js
@@ -0,0 +1,12 @@
+const GITHUB_API = 'https://api.github.com'
+const API_VERSION = '2022-11-28'
+
+export class GithubProjectsService{
+    constructor(token){
+        this.token = token;
+    }
+
+    async getProyects(org){
+
+    }
+}
