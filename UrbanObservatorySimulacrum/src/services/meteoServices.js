f3602043d846927a3c97c3ebaaf8b8d4040485f0
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-01-25 17:49:13 -0500

    carga de servicios de meteo con caché

--- UrbanObservatorySimulacrum/src/services/meteoServices.js
+++ UrbanObservatorySimulacrum/src/services/meteoServices.js
@@ -1,4 +1,4 @@
-export async function getWeather(long,lat) {
+/* export async function getWeather(long,lat) {
     try {
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,wind_speed_10m,precipitation`)
         const data = await response.json()
@@ -9,3 +9,23 @@ export async function getWeather(long,lat) {
     }
 }
 
+ */
+const weatherCache = new Map(); // Crear una caché en memoria para almacenar los datos meteorológicos
+export async function getWeather(long,lat) {
+    const cacheKey = `${long},${lat}`; //Crear una clave única para la caché basada en las coordenadas
+    if (weatherCache.has(cacheKey)) { // Verificar si los datos ya están en la caché
+        return weatherCache.get(cacheKey); // Si existen, devolver los datos almacenados, sin necesidad de hacer una nueva solicitud
+    }
+
+    //Si no existen en la caché, hacer la solicitud a la API
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,wind_speed_10m,precipitation`)
+        const data = await response.json()
+
+        weatherCache.set(cacheKey, data);// Almacenar los datos obtenidos en la caché antes de devolverlos
+        
+        return data
+    } catch (error) {
+        console.error(error);
+    }
+}
